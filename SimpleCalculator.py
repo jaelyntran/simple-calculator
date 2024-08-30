8820e46218b09723d3752e3254189afed8e96a90
jaelyntran <70259055+jaelyntran@users.noreply.github.com>
2024-08-29 21:14:20 -0700

    Added parse_equation function to parse user_input string of mathematical expression into a list of floats and strings of operators accordingly. Added calculate_expression function to calculate the parsed expression by performing division, multiplication, and percentage operations before addition and subtraction operations. After each calculation, the returned value is inserted to the list where the operands and operator are removed. Return when list is left with one final value. Updated operator helper functions name and added divide by zero error handling

--- SimpleCalculator.py
+++ SimpleCalculator.py
@@ -1,20 +1,84 @@
-def addition(number1, number2):
-    return number1 + number2
-def subtraction(number1, number2):
-    return number1 - number2
+import re
+import sys
 
-def multiplication(number1, number2):
-    result = round(number1 * number2, 8)
+
+def parse_equation(user_input):
+    # Define a pattern of number
+    # or operators
+    pattern = r'\d+\.?\d*|[-+*/%]'
+
+    matches = re.findall(pattern, user_input)
+
+    # Convert numbers to floats
+    matches = [float(x) if re.match(r'\d+\.?\d*', x)
+               else x for x in matches]
+
+    return matches
+
+
+def calculate_expression(expression):
+    # Process division, subtraction, and percentage first
+    while any(operation in expression for operation in ['*', '/', '%']):
+        for idx, val in enumerate(expression[:-1]):
+            if val in ['*', '/', '%']:
+                num1 = expression[idx - 1]
+                num2 = None
+                result = None
+                if val != '%':
+                    num2 = expression[idx + 1]
+
+                if val == '*':
+                    result = multiply(num1, num2)
+                elif val == '/':
+                    result = divide(num1, num2)
+                elif val == '%':
+                    result = percentage_of(num1)
+
+                expression[idx - 1] = float(result)
+                del expression[idx:idx + 2]
+                break
+
+    # Process addition and subtraction
+    while len(expression) > 1:
+        num1 = expression[0]
+        num2 = expression[2]
+        result = None
+
+        if expression[1] == '+':
+            result = add(num1, num2)
+        elif expression[1] == '-':
+            result = subtract(num1, num2)
+
+        equation = [float(result)] + expression[3:]
+
+    return expression[0]
+
+
+def add(num1, num2):
+    return num1 + num2
+
+
+def subtract(num1, num2):
+    return num1 - num2
+
+
+def multiply(num1, num2):
+    result = round(num1 * num2, 8)
     return f"{result:.8f}"
 
-def division(number1, number2):
-    result = round(number1 / number2, 8)
+
+def divide(num1, num2):
+    if num2 == 0:
+        print("Error: Cannot divide by zero.")
+        sys.exit(1)
+    result = round(num1 / num2, 8)
     return f"{result:.8f}"
 
-def percentage(number):
-    result = round(number / 100, 8)
+
+def percentage_of(num):
+    result = round(num / 100, 8)
     return f"{result:.8f}"
+
 
 def sign_inversion(number):
     return number * -1
-
--- main.py
+++ main.py
@@ -1,5 +1,19 @@
-def main():
+import SimpleCalculator
 
+
+def main():
+    user_input = input("Enter an equation: ")
+
+    # Parse user input into a list
+    # of equation elements
+    expression = SimpleCalculator.parse_equation(user_input)
+
+    # Process tuple by precedence
+    result = SimpleCalculator.calculate_expression(expression)
+    if result.is_integer():
+        print(f"= {int(result)}")
+    else:
+        print(f"= {result}")
 
 
 if __name__ == "__main__":
